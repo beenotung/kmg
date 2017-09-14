24f1f9b6d373bdbdf483eec57700688706b3c628
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-09-14 17:06:30 +0800

    fixed cyclic dependency issue

--- src/model/api/tables.ts
+++ src/model/api/tables.ts
@@ -2,8 +2,6 @@ import {Horizon, OldRecord, TableObject} from "typestub-horizon-client";
 import {Type} from "@beenotung/tslib/lang";
 import {BaseDBObject} from "./base/base-db-object";
 import {BaseSearchObject} from "./base/base-search-object";
-import {iteratorsToArray} from "@beenotung/tslib";
-import "./custom/index";
 
 export interface Table {
   tableName: string;
@@ -12,11 +10,8 @@ export interface Table {
 
 export type TableType<A extends BaseDBObject> = Table & Type<A>;
 
-const tables = new Set<TableType<BaseDBObject>>();
-
-export function allTables(): string[] {
-  return iteratorsToArray([tables.values()]).map(x => x.tableName);
-}
+/* protected, package-level access only */
+export const tables = new Set<TableType<BaseDBObject>>();
 
 /**@deprecated*/
 export function toTable<A extends BaseDBObject>(hz: Horizon, table: TableType<A>): TableObject<A> {
