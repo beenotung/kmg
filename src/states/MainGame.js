7b98ff2b033e18cb276e92a91451114b99375a66
januslin1 <januslinhc@hotmail.com>
2017-11-25 02:31:05 +0800

    Update card check

--- src/states/MainGame.js
+++ src/states/MainGame.js
@@ -553,7 +553,7 @@ export default class extends Phaser.State {
     }
 
     function getRiskCard () {
-      return that.game.global.game.currentBackpackCardList.filter(x => x.type == "risk")
+      return that.game.global.game.currentBackpackCardList.filter(x => (x.type == "risk" || x.type == "transient_profit"))
     }
 
     function redrawCardListNextPage() {
