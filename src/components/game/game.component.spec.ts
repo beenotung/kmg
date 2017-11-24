7f5e07c3ab77e17829a15a7194a01769a03b068d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-11-25 04:02:49 +0800

    fixed for tslint

--- src/components/game/game.component.spec.ts
+++ src/components/game/game.component.spec.ts
@@ -1,8 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { GameComponent } from './game.component';
+import { GameComponent } from "./game.component";
 
-describe('GameComponent', () => {
+describe("GameComponent", () => {
   let component: GameComponent;
   let fixture: ComponentFixture<GameComponent>;
 
@@ -19,7 +19,7 @@ describe('GameComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
